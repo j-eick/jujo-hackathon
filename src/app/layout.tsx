aea30367e4b5b41ad0ac1020527f9f3a48ef85a1
juli <juli@polychainmonsters.com>
2023-06-22 18:12:12 +0200

    please my coworker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "JuJo Hackathon",
+  title: "JuJo Hackathon Leckmichdoch",
   description: "Let's build something cool together!",
 };
 
