7d3baa640f92ab61879f54d68ebfc336cf30f931
Joschka Eickhoff <jeickhoff@posteo.de>
2023-06-24 21:11:41 +0200

    comp: Drama Title, Desciption Pictures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,15 @@
 "use client"; // dirty fix? <-- https://stackoverflow.com/questions/74614922/super-expression-must-either-be-null-or-a-function-nextjs-13
-import Image from "next/image";
 import styles from "./page.module.css";
 import Button from "./components/button";
 import Input from "./components/input";
+import ProductImage from "./components/image";
+
+console.clear();
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          By{" "}
-          <Image
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            className={styles.vercelLogo}
-            width={100}
-            height={24}
-            priority
-          />
-        </a>
-      </div>
-
+      <Input value="Hello World" onChange={(e) => console.log({ e })} />
       <div
         style={{
           display: "flex",
@@ -33,33 +18,41 @@ export default function Home() {
           padding: "24px",
         }}
       >
-        <Input value="Hello World" onChange={(e) => console.log({ e })} />
+        <h1>Leather Jacket</h1>
+        <ul className={styles.drama_description_list}>
+          <li className={styles.drama_description_item}>
+            <p className={styles.p}>Drama: Descendants of the sun, S1/EP2</p>
+          </li>
+          <li className={styles.drama_description_item}>
+            <p className={styles.p}>Actors: Park Seo-Jun, as Minho</p>
+          </li>
+        </ul>
+        <ul className={styles.ul}>
+          <li>
+            <ProductImage src={require("./../../public/jacket.png")} alt="stuff" width={150} height={150} />
+          </li>
+          <li>
+            <ProductImage src={require("./../../public/jacket.png")} alt="stuff" width={150} height={150} />
+          </li>
+        </ul>
         <Button type="primary" onClick={() => {}} className={styles.testButton}>
-          Primary Active
+          Amazon
         </Button>
-        <Button
-          type="primary"
-          disabled
-          onClick={() => {}}
-          className={styles.testButton}
-        >
+        <Button type="primary" onClick={() => {}} className={styles.testButton}>
+          Ebay
+        </Button>
+        <Button type="primary" onClick={() => {}} className={styles.testButton}>
+          yo mama
+        </Button>
+        {/* <Button type="primary" disabled onClick={() => {}} className={styles.testButton}>
           Primary Disabled
         </Button>
-        <Button
-          type="secondary"
-          onClick={() => {}}
-          className={styles.testButton}
-        >
+        <Button type="secondary" onClick={() => {}} className={styles.testButton}>
           Secondary Active
         </Button>
-        <Button
-          type="secondary"
-          disabled
-          onClick={() => {}}
-          className={styles.testButton}
-        >
+        <Button type="secondary" disabled onClick={() => {}} className={styles.testButton}>
           Secondary Disabled
-        </Button>
+        </Button> */}
       </div>
     </main>
   );
